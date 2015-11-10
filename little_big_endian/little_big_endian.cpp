33652da4d569622f16e1c0f2f57075b50bf56e76
starlitnext <vipxxq@foxmail.com>
2015-11-10 20:39:01 +0800

    add little big endian

---
+++ little_big_endian/little_big_endian.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+	int i;
+	for (i = 0; i < len; i++)
+		printf(" %.2x", start[i]);
+	printf("\n");
+}
+
+void show_int(int x) {
+	show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_float(float x) {
+	show_bytes((byte_pointer) &x, sizeof(x));
+}
+
+void show_pointer(void *x) {
+	show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val) {
+	int ival = val;
+	float fval = (float) val;
+	int *pval = &ival;
+	show_int(ival);
+	show_float(fval);
+	show_pointer(pval);
+}
+
+int main() {
+	int val = 12345;
+	printf("x = %x\n", val);
+	test_show_bytes(val);
+	return 0;
+}
