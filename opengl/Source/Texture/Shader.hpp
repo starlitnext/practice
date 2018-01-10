a1f952fa4bcb00ee2ab7868353bead053bad6c73
unknown <gzxuxiaoqiang@game.ntes>
2018-01-10 19:51:44 +0800

    # opengl Texture Transform

---
+++ opengl/Source/Texture/Shader.hpp
@@ -0,0 +1,21 @@
+#ifndef _SHADER_HPP_
+#define _SHADER_HPP_
+
+#include "Common.hpp"
+
+class Shader {
+public:
+	// Constructor generates the shader on the fly
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+
+	// Uses the current shader
+	void Use();
+
+	GLuint Program();
+
+private:
+	GLuint programId_;
+};
+
+#endif // !_SHADER_HPP_
+
