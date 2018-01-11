e0cf1e23f119062038b84a7dd0e8a4897a0bc3eb
unknown <gzxuxiaoqiang@game.ntes>
2018-01-11 18:41:21 +0800

    # opengl Cube

---
+++ opengl/Source/Cube/Common.hpp
@@ -0,0 +1,17 @@
+#ifndef _COMMON_HPP_
+#define _COMMON_HPP_
+
+#include <string>
+
+// #define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "SOIL.lib")
+
+#endif // !_COMMON_HPP_
+
