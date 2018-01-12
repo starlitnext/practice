dbba56fae6e74f5105e31f3ecfee15e9313ef766
unknown <gzxuxiaoqiang@game.ntes>
2018-01-12 16:50:54 +0800

    # opengl Camera

---
+++ opengl/Source/Camera/Common.hpp
@@ -0,0 +1,23 @@
+#ifndef _COMMON_HPP_
+#define _COMMON_HPP_
+
+#include <string>
+
+// #define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <SOIL.h>
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "SOIL.lib")
+
+#endif // !_COMMON_HPP_
+
